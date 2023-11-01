ceb97d032f3a6ffc73380a2e2fa5ad6f77ae8a4c
uday-dev967 <udaychakravarthi22@gmail.com>
2023-11-01 16:17:13 +0530

    feature: added store modules all basic tables

--- store/index.js
+++ store/index.js
@@ -3,6 +3,15 @@ import * as productTypes from "~/store/modules/productTypes.js"
 import * as materialTypes from "~/store/modules/materialTypes.js"
 import * as connectionTypes from "~/store/modules/connectionTypes.js"
 import * as masterPipe from "~/store/modules/masterPipe.js"
+import * as commonProductFields from "~/store/modules/commonProductFields.js"
+import * as pipeParameters from "~/store/modules/pipeParameters,js"
+import * as pipeFittingTypes from "~/store/modules/pipeFittingTypes.js"
+import * as normalPipeFittingParameters from "~/store/modules/normalPipeFittingParameters.js"
+import * as specialPipeFittingParameters from "~/store/modules/specialPipeFittingParameters.js"
+import * as dWVPipeFittingParameters from "~/store/modules/dWVPipeFittingParameters.js"
+import * as plumbingFixtureParameters from "~/store/modules/plumbingFixtureParameters.js"
+import * as valveParameters from "~/store/modules/valveParameters.js"
+import * as mechanicalEquipmentParameters from "~/store/modules/mechanicalEquipmentParameters.js"
 
 export default {
 	state() {
@@ -17,5 +26,14 @@ export default {
 		materialTypes,
 		connectionTypes,
 		masterPipe,
+		commonProductFields,
+		pipeParameters,
+		pipeFittingTypes,
+		normalPipeFittingParameters,
+		specialPipeFittingParameters,
+		dWVPipeFittingParameters,
+		plumbingFixtureParameters,
+		valveParameters,
+		mechanicalEquipmentParameters,
 	},
 }
---
+++ store/modules/mechanicalEquipmentParameters.js
@@ -0,0 +1,55 @@
+import { fetchData, deleteItems, fetchAllData, addItem, updateItem } from "./moduleHelper.js"
+import ApiService from "~/services/mechanicalEquipmentParametersServices.js"
+
+const baseName = "MechanicalEquipmentParameters"
+const _baseName = "mechanicalEquipmentParameters"
+const mutationTypes = {
+	setData: `set${baseName}Data`,
+	setTotal: `setTotal${baseName}`,
+}
+
+export const namespaced = true
+
+export const state = () => {
+	return {
+		[`${_baseName}Data`]: {},
+		error: {},
+		[`total${baseName}`]: 0,
+	}
+}
+
+export const mutations = {
+	[mutationTypes.setData](state, data) {
+		state[`${_baseName}Data`] = data
+	},
+	[mutationTypes.setTotal](state, total) {
+		state[`total${baseName}`] = total
+	},
+}
+
+export const actions = {
+	fetchTableData({ commit }, params) {
+		return fetchData(commit, ApiService[`get${baseName}`], params, mutationTypes)
+	},
+	fetchAllRecords({ commit }) {
+		return fetchAllData(commit, ApiService[`getAll${baseName}`], mutationTypes)
+	},
+
+	addTableData({ commit }, data) {
+		return addItem(ApiService[`post${baseName}`], data)
+	},
+	updateTableData({ commit }, data) {
+		return updateItem(ApiService[`update${baseName}`], data)
+	},
+	deleteTableData({ commit }, data) {
+		return deleteItems(ApiService[`delete${baseName}`], data)
+	},
+}
+
+export const getters = {
+	[`get${baseName}`](state) {
+		// eslint-disable-next-line no-console
+		console.log("from getter", state[`${_baseName}Data`])
+		return state[`${_baseName}Data`][`${"connections"}Data`]
+	},
+}
